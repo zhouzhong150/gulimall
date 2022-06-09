9f767aac8bef67a7b92d0ad86df6f8e1966e8a8a
zhouzhong <1097449124@qq.com>
2022-06-09 16:34:46 +0800

    init porject

---
+++ gulimall-coupin/src/test/java/com/zz/gulimall/gulimallcoupin/GulimallCoupinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zz.gulimall.gulimallcoupin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallCoupinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
